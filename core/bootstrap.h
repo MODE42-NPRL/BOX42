bf210a562366d6be182723b57f10e97fc59b4810
MODE42-NPRL <info@mode42.com>
2026-05-16 09:23:11 +0000

    Create bootstrap.h

---
+++ core/bootstrap.h
@@ -0,0 +1,9 @@
+#ifndef BOX42_BOOTSTRAP_H
+#define BOX42_BOOTSTRAP_H
+
+/* Initialize full BOX42 directory layout.
+ * Returns 0 on success, -1 on error.
+ */
+int box42_bootstrap(void);
+
+#endif /* BOX42_BOOTSTRAP_H */
